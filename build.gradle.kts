0445ce69dcde9db1c2d9dad9f23595506005a461
BujjuIsABee <bujjuinquiries@gmail.com>
2026-06-09 12:29:01 -0500

    Configure Gradle build settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,21 @@ kotlin {
     jvmToolchain(21)
 }
 
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.group_finity.mascot.MainKt"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    from (sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
+
 tasks.test {
     useJUnitPlatform()
 }
