0d76abd3c9b9ce6648693efef8efe08445949355
BujjuIsABee <bujjuinquiries@gmail.com>
2026-07-13 20:29:22 -0500

    Add log formatter

---
+++ src/main/kotlin/com/group_finity/mascot/LogFormatter.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2026, Bujju
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+ * following conditions are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright notice, this list of conditions and the
+ *        following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+ *        following disclaimer in the documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+ *        products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.group_finity.mascot
+
+import java.time.Instant
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.logging.LogRecord
+import java.util.logging.SimpleFormatter
+
+class LogFormatter : SimpleFormatter() {
+    private val dateFormatter = DateTimeFormatter
+        .ofLocalizedDateTime(FormatStyle.MEDIUM)
+        .withZone(ZoneId.systemDefault())
+
+    override fun format(record: LogRecord) = buildString {
+        append(dateFormatter.format(Instant.ofEpochMilli(record.millis)))
+        append(" ")
+        append(record.level.localizedName)
+        append(": ")
+        append(record.sourceClassName ?: record.loggerName)
+        append(" ")
+        append(record.sourceMethodName ?: "")
+        append(" ")
+        append(formatMessage(record))
+        append(System.lineSeparator())
+        append(record.thrown?.stackTraceToString() ?: "")
+    }
+}
